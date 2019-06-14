d529d98f8d880e8dcb6bd98a51f436671074fd78
Duane Robertson <duane@duanerobertson.com>
2019-06-14 09:36:58 -0500

    Add blueberry pies.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # D Pies
 
-This is a fairly simple mod that adds highly nutritious apple and meat pies. Mobs redo and Big Trees are recommended.
+This is a fairly simple mod that adds highly nutritious fruit and meat pies. The Nmobs and Big Trees mods are helpful.
 
-Uncooked apple pies can be made with two apples, a flour, and either three syrup or one honey or one bucket of milk. Meat pies require two raw meat, an onion, and a flour. They must be baked to be edible.
+Uncooked apple pies can be made with two apples (or pears), a flour, and either three syrup or one honey or one bucket of milk. Uncooked blueberry pies can be made with six blueberries, two flour, and one honey. Meat pies require two raw meat, an onion, and a flour. They must be baked to be edible.
 
 Onions can be gathered from tall grass, and grown like any other crop. There are no onion seeds -- just plant an onion.
 
@@ -11,6 +11,6 @@ The source is available on github.
 
 Code: LGPL2, Textures: CC by SA and CC0
 
-Mod dependencies: default, farming, vessels, bonemeal?, mobs?, big_trees?
+Mod dependencies: default, farming, vessels, bonemeal?, nmobs?, big_trees?
 
 Download: https://github.com/duane-r/dpies/archive/master.zip
--- init.lua
+++ init.lua
@@ -3,68 +3,102 @@
 -- Distributed under the LGPLv2.1 (https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html)
 
 dpies_mod = {}
-dpies_mod.version = "20171028"
-dpies_mod.path = minetest.get_modpath(minetest.get_current_modname())
-dpies_mod.world = minetest.get_worldpath()
+local mod = dpies_mod
+local mod_name = 'dpies'
+mod.version = '20171028'
+mod.path = minetest.get_modpath(minetest.get_current_modname())
+mod.world = minetest.get_worldpath()
 
 
-minetest.register_craftitem("dpies:apple_pie_slice", {
-	description = "Apple Pie Slice",
-	inventory_image = "dpies_apple_pie_slice.png",
+minetest.register_craftitem(mod_name..':apple_pie_slice', {
+	description = 'Apple Pie Slice',
+	inventory_image = 'dpies_apple_pie_slice.png',
 	on_use = minetest.item_eat(5),
 })
 
 minetest.register_craft({
-	output = 'dpies:apple_pie_slice 6',
+	output = mod_name..':apple_pie_slice 6',
 	type = 'shapeless',
 	recipe = {
-		'dpies:apple_pie',
+		mod_name..':apple_pie',
 	}
 })
 
-minetest.register_node("dpies:apple_pie", {
-	description = "Apple Pie",
-	drawtype = "raillike",
-	tiles = {"dpies_apple_pie.png"},
-	inventory_image  = "dpies_apple_pie.png",
-	paramtype = "light",
+minetest.register_node(mod_name..':apple_pie', {
+	description = 'Apple Pie',
+	drawtype = 'raillike',
+	tiles = {'dpies_apple_pie.png'},
+	inventory_image  = 'dpies_apple_pie.png',
+	paramtype = 'light',
 	walkable = false,
 	selection_box = {
-		type = "fixed",
+		type = 'fixed',
 		fixed = {-0.4, -0.5, -0.4, 0.5, -0.4, 0.4}
 	},
 	groups = {dig_immediate = 3, attached_node = 1},
 	sounds = default.node_sound_dirt_defaults(),
 })
 
-minetest.register_craftitem("dpies:apple_pie_uncooked", {
-	description = "Uncooked Apple Pie",
-	inventory_image = "dpies_apple_pie_uncooked.png",
+minetest.register_craftitem(mod_name..':apple_pie_uncooked', {
+	description = 'Uncooked Apple Pie',
+	inventory_image = 'dpies_apple_pie_uncooked.png',
 })
 
 if minetest.registered_items['big_trees:syrup'] then
-  minetest.register_craft({
-    output = 'dpies:apple_pie_uncooked',
-    type = 'shapeless',
-    recipe = {
-      'default:apple',
-      'default:apple',
-      'farming:flour',
-      'big_trees:syrup',
-      'big_trees:syrup',
-      'big_trees:syrup',
-    },
-    replacements = {
-      {'dpies:syrup', 'vessels:glass_bottle'},
-      {'dpies:syrup', 'vessels:glass_bottle'},
-      {'dpies:syrup', 'vessels:glass_bottle'},
-    },
-  })
+	minetest.register_craft({
+		output = mod_name..':apple_pie_uncooked',
+		type = 'shapeless',
+		recipe = {
+			'default:apple',
+			'default:apple',
+			'farming:flour',
+			'big_trees:syrup',
+			'big_trees:syrup',
+			'big_trees:syrup',
+		},
+		replacements = {
+			{mod_name..':syrup', 'vessels:glass_bottle'},
+			{mod_name..':syrup', 'vessels:glass_bottle'},
+			{mod_name..':syrup', 'vessels:glass_bottle'},
+		},
+	})
+
+	minetest.register_craft({
+		output = mod_name..':apple_pie_uncooked',
+		type = 'shapeless',
+		recipe = {
+			'environ:pear',
+			'environ:pear',
+			'farming:flour',
+			'big_trees:syrup',
+			'big_trees:syrup',
+			'big_trees:syrup',
+		},
+		replacements = {
+			{mod_name..':syrup', 'vessels:glass_bottle'},
+			{mod_name..':syrup', 'vessels:glass_bottle'},
+			{mod_name..':syrup', 'vessels:glass_bottle'},
+		},
+	})
 end
 
 if minetest.registered_items['mobs:bucket_milk'] then
 	minetest.register_craft({
-		output = 'dpies:apple_pie_uncooked',
+		output = mod_name..':apple_pie_uncooked',
+		type = 'shapeless',
+		recipe = {
+			'environ:pear',
+			'environ:pear',
+			'farming:flour',
+			'mobs:bucket_milk',
+		},
+		replacements = {
+			{'mobs:bucket_milk', 'bucket:bucket_empty'},
+		},
+	})
+
+	minetest.register_craft({
+		output = mod_name..':apple_pie_uncooked',
 		type = 'shapeless',
 		recipe = {
 			'default:apple',
@@ -73,14 +107,14 @@ if minetest.registered_items['mobs:bucket_milk'] then
 			'mobs:bucket_milk',
 		},
 		replacements = {
-			{'mobs:bucket_milk', 'dpies:bucket_empty'},
+			{'mobs:bucket_milk', 'bucket:bucket_empty'},
 		},
 	})
 end
 
 if minetest.registered_items['mobs:honey'] then
 	minetest.register_craft({
-		output = 'dpies:apple_pie_uncooked',
+		output = mod_name..':apple_pie_uncooked',
 		type = 'shapeless',
 		recipe = {
 			'default:apple',
@@ -89,34 +123,113 @@ if minetest.registered_items['mobs:honey'] then
 			'mobs:honey',
 		},
 	})
+
+	minetest.register_craft({
+		output = mod_name..':apple_pie_uncooked',
+		type = 'shapeless',
+		recipe = {
+			'environ:pear',
+			'environ:pear',
+			'farming:flour',
+			'mobs:honey',
+		},
+	})
+end
+
+
+minetest.register_craftitem(mod_name..':blueberry_pie_slice', {
+	description = 'Blueberry Pie Slice',
+	inventory_image = 'dpies_blueberry_pie_slice.png',
+	on_use = minetest.item_eat(5),
+})
+
+minetest.register_craft({
+	output = mod_name..':blueberry_pie_slice 6',
+	type = 'shapeless',
+	recipe = {
+		mod_name..':blueberry_pie',
+	}
+})
+
+minetest.register_node(mod_name..':blueberry_pie', {
+	description = 'Blueberry Pie',
+	drawtype = 'raillike',
+	tiles = {'dpies_blueberry_pie.png'},
+	inventory_image  = 'dpies_blueberry_pie.png',
+	paramtype = 'light',
+	walkable = false,
+	selection_box = {
+		type = 'fixed',
+		fixed = {-0.4, -0.5, -0.4, 0.5, -0.4, 0.4}
+	},
+	groups = {dig_immediate = 3, attached_node = 1},
+	sounds = default.node_sound_dirt_defaults(),
+})
+
+minetest.register_craftitem(mod_name..':blueberry_pie_uncooked', {
+	description = 'Uncooked Blueberry Pie',
+	inventory_image = 'dpies_blueberry_pie_uncooked.png',
+})
+
+if minetest.registered_items['mobs:honey'] then
+	minetest.register_craft({
+		output = mod_name..':blueberry_pie_uncooked',
+		type = 'shapeless',
+		recipe = {
+			'default:blueberries',
+			'default:blueberries',
+			'default:blueberries',
+			'default:blueberries',
+			'default:blueberries',
+			'default:blueberries',
+			'farming:flour',
+			'farming:flour',
+			'mobs:honey',
+		},
+	})
+else
+	minetest.register_craft({
+		output = mod_name..':blueberry_pie_uncooked',
+		type = 'shapeless',
+		recipe = {
+			'default:blueberry',
+			'default:blueberry',
+			'default:blueberry',
+			'default:blueberry',
+			'default:blueberry',
+			'default:blueberry',
+			'farming:flour',
+			'farming:flour',
+		},
+	})
 end
 
 if minetest.registered_items['mobs:meat_raw'] then
-	minetest.register_craftitem("dpies:meat_pie_uncooked", {
-		description = "Uncooked Meat Pie",
-		inventory_image = "dpies_meat_pie_uncooked.png",
+	minetest.register_craftitem(mod_name..':meat_pie_uncooked', {
+		description = 'Uncooked Meat Pie',
+		inventory_image = 'dpies_meat_pie_uncooked.png',
 	})
 
 	minetest.register_craft({
-		output = 'dpies:meat_pie_uncooked',
+		output = mod_name..':meat_pie_uncooked',
 		type = 'shapeless',
 		recipe = {
 			'mobs:meat_raw',
 			'mobs:meat_raw',
-			'dpies:onion',
+			mod_name..':onion',
 			'farming:flour',
 		},
 	})
 
-	minetest.register_node("dpies:meat_pie", {
-		description = "Meat Pie",
-		drawtype = "raillike",
-		tiles = {"dpies_meat_pie.png"},
-		inventory_image  = "dpies_meat_pie.png",
-		paramtype = "light",
+	minetest.register_node(mod_name..':meat_pie', {
+		description = 'Meat Pie',
+		drawtype = 'raillike',
+		tiles = {'dpies_meat_pie.png'},
+		inventory_image  = 'dpies_meat_pie.png',
+		paramtype = 'light',
 		walkable = false,
 		selection_box = {
-			type = "fixed",
+			type = 'fixed',
 			fixed = {-0.4, -0.5, -0.4, 0.5, -0.4, 0.4}
 		},
 		groups = {dig_immediate = 3, attached_node = 1},
@@ -124,79 +237,79 @@ if minetest.registered_items['mobs:meat_raw'] then
 	})
 
 	minetest.register_craft({
-		type = "cooking",
+		type = 'cooking',
 		cooktime = 15,
-		output = "dpies:meat_pie",
-		recipe = "dpies:meat_pie_uncooked"
+		output = mod_name..':meat_pie',
+		recipe = mod_name..':meat_pie_uncooked'
 	})
 
-	minetest.register_craftitem("dpies:meat_pie_slice", {
-		description = "Meat Pie Slice",
-		inventory_image = "dpies_meat_pie_slice.png",
+	minetest.register_craftitem(mod_name..':meat_pie_slice', {
+		description = 'Meat Pie Slice',
+		inventory_image = 'dpies_meat_pie_slice.png',
 		on_use = minetest.item_eat(9),
 	})
 
 	minetest.register_craft({
-		output = 'dpies:meat_pie_slice 5',
+		output = mod_name..':meat_pie_slice 5',
 		type = 'shapeless',
 		recipe = {
-			'dpies:meat_pie',
+			mod_name..':meat_pie',
 		}
 	})
 end
 
-farming.register_plant("dpies:onion", {
-	description = "Onion",
-	inventory_image = "dpies_onion.png",
+farming.register_plant(mod_name..':onion', {
+	description = 'Onion',
+	inventory_image = 'dpies_onion.png',
 	steps = 3,
 	minlight = 13,
 	maxlight = default.LIGHT_MAX,
-	fertility = {"grassland"}
+	fertility = {'grassland'}
 })
 
-minetest.registered_items['dpies:seed_onion'] = nil
-minetest.registered_nodes['dpies:seed_onion'] = nil
-minetest.registered_craftitems['dpies:seed_onion'] = nil
-minetest.register_alias('dpies:seed_onion', 'dpies:onion')
+minetest.registered_items[mod_name..':seed_onion'] = nil
+minetest.registered_nodes[mod_name..':seed_onion'] = nil
+minetest.registered_craftitems[mod_name..':seed_onion'] = nil
+minetest.register_alias(mod_name..':seed_onion', mod_name..':onion')
 for i = 1, 3 do
-	local onion = minetest.registered_items['dpies:onion_'..i]
+	local onion = minetest.registered_items[mod_name..':onion_'..i]
 	if onion then
 		onion.drop = {
 			max_items = i,
 			items = {
-				{ items = {'dpies:onion'}, rarity = 4 - i, },
-				{ items = {'dpies:onion'}, rarity = (4 - i) * 2, },
-				{ items = {'dpies:onion'}, rarity = (4 - i) * 4, },
+				{ items = {mod_name..':onion'}, rarity = 4 - i, },
+				{ items = {mod_name..':onion'}, rarity = (4 - i) * 2, },
+				{ items = {mod_name..':onion'}, rarity = (4 - i) * 4, },
 			},
 		}
 	end
 end
 
-minetest.register_node("dpies:onion", {
-	description = "Onion",
-	drawtype = "plantlike",
+minetest.register_node(mod_name..':onion', {
+	description = 'Onion',
+	drawtype = 'plantlike',
 	visual_scale = 0.75,
-	tiles = {"dpies_onion.png"},
-	inventory_image = "dpies_onion.png",
-	paramtype = "light",
+	tiles = {'dpies_onion.png'},
+	inventory_image = 'dpies_onion.png',
+	paramtype = 'light',
 	sunlight_propagates = true,
 	walkable = false,
 	is_ground_content = false,
 	selection_box = {
-		type = "fixed",
+		type = 'fixed',
 		fixed = {-0.2, -0.5, -0.2, 0.2, 0, 0.2}
 	},
 	fertility = {'grassland'},
 	groups = {seed = 1, fleshy = 3, dig_immediate = 3, flammable = 2},
 	on_use = minetest.item_eat(2),
 	sounds = default.node_sound_leaves_defaults(),
-	next_plant = 'dpies:onion_1',
+	next_plant = mod_name..':onion_1',
 	on_timer = farming.grow_plant,
 	minlight = 10,
 	maxlight = 15,
 
 	on_place = function(itemstack, placer, pointed_thing)
-		local stack = farming.place_seed(itemstack, placer, pointed_thing, 'dpies:onion')
+		local stack = farming.place_seed(itemstack, placer, pointed_thing, mod_name..':onion')
 		if stack then
 			return stack
 		end
@@ -206,21 +319,28 @@ minetest.register_node("dpies:onion", {
 })
 
 minetest.register_craft({
-	type = "cooking",
+	type = 'cooking',
 	cooktime = 15,
-	output = "dpies:apple_pie",
-	recipe = "dpies:apple_pie_uncooked"
+	output = mod_name..':apple_pie',
+	recipe = mod_name..':apple_pie_uncooked'
+})
+
+minetest.register_craft({
+	type = 'cooking',
+	cooktime = 15,
+	output = mod_name..':blueberry_pie',
+	recipe = mod_name..':blueberry_pie_uncooked'
 })
 
 
 for i = 3, 5 do
-	minetest.override_item("default:grass_" .. i, {
+	minetest.override_item('default:grass_' .. i, {
 		drop = {
 			max_items = 2,
 			items = {
-				{ items = { "default:grass_1"}, },
+				{ items = { 'default:grass_1'}, },
 				{ items = {'farming:seed_wheat'},rarity = 5 },
-				{ items = {"dpies:onion",}, rarity = 5 },
+				{ items = {mod_name..':onion',}, rarity = 5 },
 			},
 		},
 	})
@@ -228,22 +348,22 @@ end
 
 
 if minetest.get_modpath('bonemeal') then
-  bonemeal:add_crop({{'dpies:onion_', 3, 'dpies:onion'},})
+	bonemeal:add_crop({{mod_name..':onion_', 3, mod_name..':onion'},})
 end
 
 
 if minetest.get_modpath('inspire') then
 	minetest.register_craft({
-		output = 'dpies:onion 3',
+		output = mod_name..':onion 3',
 		type = 'shapeless',
 		recipe = {
-			'dpies:onion',
+			mod_name..':onion',
 			'inspire:inspiration',
 		}
-  })
+	})
 
 	minetest.register_craft({
-		output = 'dpies:apple_pie',
+		output = mod_name..':apple_pie',
 		type = 'shapeless',
 		recipe = {
 			'default:apple',
@@ -252,10 +372,10 @@ if minetest.get_modpath('inspire') then
 			'inspire:inspiration',
 			'inspire:inspiration',
 		}
-  })
+	})
 
 	minetest.register_craft({
-		output = 'dpies:meat_pie',
+		output = mod_name..':meat_pie',
 		type = 'shapeless',
 		recipe = {
 			'mobs:meat_raw',
@@ -264,5 +384,5 @@ if minetest.get_modpath('inspire') then
 			'inspire:inspiration',
 			'inspire:inspiration',
 		}
-  })
+	})
 end
